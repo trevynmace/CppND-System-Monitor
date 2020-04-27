f24fc904c5bb6712952c77b94d1a18cb95455e5a
Trevyn Mace <trevynmace@gmail.com>
2020-04-27 20:02:20 +0000

    removing pointless comment

--- src/processor.cpp
+++ src/processor.cpp
@@ -11,7 +11,7 @@ float Processor::Utilization()
 {
     std::vector<string> cpu = LinuxParser::CpuUtilization();
     long idle = stol(cpu[3]) + stol(cpu[4]);
-    long active = stol(cpu[1]) + stol(cpu[2]) + stol(cpu[5]) + stol(cpu[6]) + stol(cpu[7]); //cpu[0] is empty? causing a crash.. hmm
+    long active = stol(cpu[1]) + stol(cpu[2]) + stol(cpu[5]) + stol(cpu[6]) + stol(cpu[7]);
     long total = idle + active;
     return (float) active / (float) total;
 }
